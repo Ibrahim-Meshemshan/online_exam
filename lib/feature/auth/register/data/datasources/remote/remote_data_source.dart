35634798b5490e37ce15d842d7db3793948c36ea
Abdelrhman-18 <abdelrhmanv61@gmail.com>
2025-03-11 01:56:56 +0200

    fix(signup): fix issue in the signup page feat(ui): improve home screen UI feat: add BottomNavigationBar
    Fixed a bug in the signup page that was preventing users from proceeding.
    Also improved the UI design and added a bottom navigation bar for easier navigation.

--- lib/feature/auth/register/data/datasources/remote/remote_data_source.dart
+++ lib/feature/auth/register/data/datasources/remote/remote_data_source.dart
@@ -1,11 +1,10 @@
 import 'package:dartz/dartz.dart';
-
 import 'package:injectable/injectable.dart';
-import '../../../../../../core/api_manager/api_manager.dart';
-import '../../../../../../core/utils/failures.dart';
-import '../../models/register_requst_model.dart';
-import '../../models/register_response_model.dart';
-import '../contract/contract_data_sourc.dart';
+import 'package:online_exam/core/api_manager/api_manager.dart';
+import 'package:online_exam/core/utils/failures.dart';
+import 'package:online_exam/feature/auth/register/data/datasources/contract/contract_data_sourc.dart';
+import 'package:online_exam/feature/auth/register/data/models/register_requst_model.dart';
+import 'package:online_exam/feature/auth/register/data/models/register_response_model.dart';
 
 @Injectable(as: ContractDataSource)
 class RemoteRegisterDataSource implements ContractDataSource {
--- lib/feature/auth/register/presentation/pages/register_screen.dart
+++ lib/feature/auth/register/presentation/pages/register_screen.dart
@@ -1,209 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:online_exam/core/di/di.dart';
 import 'package:online_exam/feature/app/presentation/pages/home_screen.dart';
-import '../../../../../core/di/di.dart';
-import '../../../../../core/utils/dialog.dart';
-import '../../../../../core/utils/theme_manager.dart';
-import '../../../login/presentation/widgets/custom_text_form_field.dart';
-import '../cubit/register_cubit.dart';
+import 'package:online_exam/feature/auth/register/presentation/cubit/register_cubit.dart';
+import 'package:online_exam/feature/auth/register/presentation/widget/register_form.dart';
 
-class RegisterScreen extends StatefulWidget {
+
+class RegisterScreen extends StatelessWidget {
   static const String routeName = 'register_screen';
-
   const RegisterScreen({super.key});
 
-  @override
-  State<RegisterScreen> createState() => _RegisterScreenState();
-}
-
-class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => getIt<RegisterCubit>(),
       child: Builder(
         builder: (context) {
-          final viewModel = context.read<RegisterCubit>();
-
-          return Scaffold(
-            appBar: AppBar(title: const Text("Register")),
-            body: BlocListener<RegisterCubit, RegisterState>(
-              listener: (context, state) {
-                if (state is RegisterLoading) {
-                  DialogUtils.showLoading(context, 'Loading...');
-                } else {
-                  DialogUtils.hideLoading(context);
-                  if (state is RegisterFailure) {
-                    DialogUtils.showMessage(
-                        context, state.errorMessage ?? 'Registration failed');
-                  } else if (state is RegisterSuccess) {
-                    DialogUtils.showMessage(context, 'Registered Successfully');
-                    Navigator.pop(context);
+          return Scaffold( //
+            appBar: AppBar(title: const Text("Sign up")),
+            body: Padding(
+              padding:  EdgeInsets.all(16.0.sp),
+              child: BlocListener<RegisterCubit, RegisterState>(
+                listener: (context, state) {
+                  if (state is RegisterSuccess) {
+                    Navigator.pushNamed(
+                        context,HomeScreen.routeName);
+                  } else if (state is RegisterFailure) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(state.errorMessage),
+                        backgroundColor: Colors.red,
+                        duration: const Duration(seconds: 3),
+                      ),
+                    );
                   }
-                }
-              },
-              child: Padding(
-                padding: EdgeInsets.all(16.sp),
-                child: Form(
-                  key: viewModel.formKey,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        CustomTextFormField(
-                          controller: viewModel.userNameController,
-                          labelText: "Username",
-                          hintText: "Enter your username",
-                          keyboardType: TextInputType.text,
-                          validator: (value) => value!.isEmpty
-                              ? 'This user name is not valid'
-                              : null,
-                        ),
-                        15.verticalSpace,
-                        Row(
-                          children: [
-                            Expanded(
-                              child: CustomTextFormField(
-                                controller: viewModel.firstNameController,
-                                labelText: "First Name",
-                                hintText: "Enter first name",
-                                keyboardType: TextInputType.text,
-                                validator: (value) => value!.isEmpty
-                                    ? 'Enter a first name'
-                                    : null,
-                              ),
-                            ),
-                            10.horizontalSpace,
-                            Expanded(
-                              child: CustomTextFormField(
-                                controller: viewModel.lastNameController,
-                                labelText: "Last Name",
-                                hintText: "Enter last name",
-                                keyboardType: TextInputType.text,
-                                validator: (value) =>
-                                    value!.isEmpty ? 'Enter a last name' : null,
-                              ),
-                            ),
-                          ],
-                        ),
-                        15.verticalSpace,
-                        CustomTextFormField(
-                          controller: viewModel.emailController,
-                          labelText: "Email",
-                          hintText: "Enter your Email",
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) =>
-                              viewModel.checkValidEmail(value!)
-                                  ? null
-                                  : 'Invalid email',
-                        ),
-                        15.verticalSpace,
-                        Row(
-                          children: [
-                            Expanded(
-                              child: CustomTextFormField(
-                                controller: viewModel.passwordController,
-                                labelText: "Password",
-                                hintText: "Enter Password",
-                                isObscure: viewModel.isObscurePassword,
-                                keyboardType: TextInputType.visiblePassword,
-                                icon: IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      viewModel.isObscurePassword =
-                                      !viewModel.isObscurePassword;
-                                    });
-                                  },
-                                  icon: Icon(
-                                    viewModel.isObscurePassword
-                                        ? Icons.visibility_off_rounded
-                                        : Icons.visibility_rounded,
-                                  ),
-                                ),
-                                validator: (value) => value!.length < 6
-                                    ? 'Password too short'
-                                    : null,
-                              ),
-                            ),
-                            10.horizontalSpace,
-                            Expanded(
-                              child: CustomTextFormField(
-                                controller: viewModel.rePasswordController,
-                                labelText: "Confirm password",
-                                hintText: "Confirm password",
-                                keyboardType: TextInputType.visiblePassword,
-                                validator: (value) =>
-                                    value != viewModel.passwordController.text
-                                        ? 'Passwords do not match'
-                                        : null,
-                                isObscure: viewModel.isObscureConfirmPassword,
-                                icon: IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      viewModel.isObscureConfirmPassword =
-                                      !viewModel.isObscureConfirmPassword;
-                                    });
-                                  },
-                                  icon: Icon(
-                                    viewModel.isObscureConfirmPassword
-                                        ? Icons.visibility_off_rounded
-                                        : Icons.visibility_rounded,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        15.verticalSpace,
-                        CustomTextFormField(
-                          controller: viewModel.phoneController,
-                          labelText: "Phone number",
-                          hintText: "Enter your Phone number",
-                          keyboardType: TextInputType.phone,
-                          validator: (value) {
-                            if(value==null || value.isEmpty){
-                              return 'please enter a valid phone number';
-                            }else if(value.length<11){
-                              return 'enter Egypt phone number';
-                            }
-                            return null;
-                          },
-
-                        ),
-                        30.verticalSpace,
-                        Row(
-                          children: [
-                            Expanded(
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: ThemeManager.buttonColor,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(25.r)),
-                                  padding: EdgeInsets.all(14.sp),
-                                ),
-                                onPressed: () {
-                                  if (viewModel.formKey.currentState!
-                                      .validate()) {
-                                    viewModel.register();
-                                    Navigator.pushReplacementNamed(context, HomeScreen.routeName);
-                                  }
-
-                                },
-                                child: Text(
-                                  "Sign Up",
-                                  style: ThemeManager
-                                      .appTheme.textTheme.titleSmall
-                                      ?.copyWith(fontWeight: FontWeight.w900),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                },
+                child: const RegisterForm(),
               ),
             ),
           );
@@ -212,4 +45,3 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 }
-//git config --global --add safe.directory "C:/المسار_الكامل_لمجلد_المشروع"
