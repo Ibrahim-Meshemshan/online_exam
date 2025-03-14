8327139181a6ad20ad1dc183f9465cb780f3b26f
ibrahim_meshmeshan <ibrahimmeshmeshan@gmail.com>
2025-03-15 01:18:49 +0200

    feature(edit_profile) : add function check valid email

--- lib/feature/auth/edit_profile/presentation/cubit/profile_view_model_cubit.dart
+++ lib/feature/auth/edit_profile/presentation/cubit/profile_view_model_cubit.dart
@@ -39,6 +39,13 @@ class ProfileViewModelCubit extends Cubit<ProfileViewModelState> {
   TextEditingController phoneController = TextEditingController();
 
   UserEntity? profile;
+  
+  bool checkValidEmail(String? email) {
+    if (email == null) return false;
+    var regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return regex.hasMatch(email);
+  }
 
 
   void getProfileCubit() async {
